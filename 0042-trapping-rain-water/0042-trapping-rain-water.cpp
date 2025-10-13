7f39d997576d008f668728a91fedd5d86077919d
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-13 16:47:59 +0530

    Time: 0 ms (100%), Space: 26 MB (79.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-
+        
         int N = height.size();
 
-        vector <int> prefix_array(N);
-        vector <int> suffix_array(N);
+        int maxheight = height[0];
+        int index = 0;
 
-        int start_val = height[0];
-
-        for(int i=1;i<N;i++){
-            prefix_array[i] = start_val;
-            if(height[i]>start_val)
-                start_val = height[i];
-        }
-
-        int end_val = height[N-1];
-
-        for(int i=N-2;i>=0;i--){
-            suffix_array[i] = end_val;
-            if(height[i]>end_val)
-                end_val = height[i];
-        }
-
-        int total_water = 0;
-
-        for(int i=1;i<N-1;i++){
-            if(height[i]<=prefix_array[i] && height[i]<=suffix_array[i]){
-                total_water += min(prefix_array[i],suffix_array[i])-height[i];
+        for(int i=0;i<N;i++){
+            if(height[i]>maxheight){
+                maxheight = height[i];
+                index = i;
             }
         }
 
-        return total_water;
+        int max1 = height[0];
+        int water = 0;
+
+        for(int i=1;i<index;i++){
+            if(max1>height[i] && maxheight>height[i]){
+                water += min(max1,maxheight) - height[i];
+            }
+            if(height[i]>max1){
+                max1 = height[i];
+            }
+        }
+
+        max1 = height[N-1];
+
+        for(int i=N-2;i>index;i--){
+            if(max1>height[i] && maxheight>height[i]){
+                water += min(max1,maxheight) - height[i];
+            }
+            if(height[i]>max1){
+                max1 = height[i];
+            }
+        }
+
+        return water;
     }
 };
