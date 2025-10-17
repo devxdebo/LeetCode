2ccd8e9a53108165d0d66d7be246033d3246702d
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-17 16:47:01 +0530

    Time: 0 ms (100%), Space: 8.3 MB (86.63%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.cpp
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.cpp
@@ -4,7 +4,8 @@ public:
 
         int st = 0;
         int end = -1;
-        map <int,string> mpp;
+
+        vector <string> ans(9,"");
 
         for(int i=0;i<s.size();i++){
             if((s[i]>=97 && s[i]<=122) || (s[i]>=65 && s[i]<=90)){
@@ -14,16 +15,17 @@ public:
                 for(int i = st;i<=end;i++){
                     temp.push_back(s[i]);
                 }
-                mpp[s[i]] = temp;
+                ans[s[i]-'1']=temp;
             }else{
                 st = i+1;
                 end = i;
             }
         }
         s = "";
-        for(auto i:mpp){
-            s += i.second;
-            s += " ";
+        for(auto i:ans){
+            if(i!=""){
+                s+=i+" ";
+            }
         }
         s.pop_back();
         return s;
