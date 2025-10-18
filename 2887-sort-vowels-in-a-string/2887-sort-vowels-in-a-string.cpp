433a6a9eb6aa7185f1786cd3a0a1caf1259181a0
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-18 10:52:30 +0530

    Time: 2 ms (98.45%), Space: 15.2 MB (96.16%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -2,25 +2,41 @@ class Solution {
 public:
     string sortVowels(string s) {
 
-        multiset <char> ms;
+        vector <int> hashu(26,0);
+        vector <int> hashl(26,0);
         
-        for(auto &i: s){
-            if(i=='A' || i=='E' || i=='I' || i=='O' || i=='U' || i=='a' || i=='e' || i=='i' || i=='o' || i=='u'){
-                ms.emplace(i);
+        for(auto &i:s){
+            if(i=='A' || i=='E' || i=='I' || i=='O' || i=='U'){
+                hashu[i-65]++;
+                i = ' ';
+            }else if(i=='a' || i=='e' || i=='i' || i=='o' || i=='u'){
+                hashl[i-97]++;
                 i = ' ';
             }
         }
 
-        auto it = ms.begin();
+        int ptr = 0;
 
-        for(auto &i:s){
-            if(i==' '){
-                i = *(it);
-                it++;
+        for(int i=0;i<26;i++){
+            while(hashu[i]!=0){
+                if(s[ptr]== ' '){
+                    s[ptr] = char(i+65);
+                    hashu[i]--;
+                }
+                ptr++;
+            }
+        }
+        for(int i=0;i<26;i++){
+            while(hashl[i]!=0){
+                if(s[ptr]== ' '){
+                    s[ptr] = char(i+97);
+                    hashl[i]--;
+                }
+                ptr++;
             }
         }
 
         return s;
-        
+
     }
 };
