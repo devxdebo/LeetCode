bc7dd670ed4f4d11eb1c61b7fa1b3ba6dabfb324
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-23 10:27:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    bool ispossible(int maxtime,vector <int> arr,int k){
-        int numofpeople = 1;
-        int N = arr.size();
-        long long sum = 0;
-        for(int i=0;i<N;i++){
-            sum += arr[i];
-            if(sum>maxtime){
-                numofpeople++;
-                if(numofpeople>k){
+    bool ispossible(int maxpages,vector <int> &arr,int k){
+        int numofstudents = 1;
+        long long totalpages = 0;
+        
+        for(int i=0;i<arr.size();i++){
+            totalpages += arr[i];
+            if(totalpages > maxpages){
+                numofstudents++;
+                if(numofstudents>k){
                     return false;
                 }
-                sum = arr[i];
+                totalpages = arr[i];
             }
         }
         return true;
     }
-    int splitArray(vector<int>& arr, int k) {
-        int N = arr.size();
-        
+    int splitArray(vector<int>& nums, int k) {
         int st = *(max_element(arr.begin(),arr.end()));
         long long end = accumulate(arr.begin(),arr.end(),0);
         long long ans;
         
         while(st<=end){
             
-            int mid = st + (end-st)/2;
+            long long mid = st + (end-st)/2;
             
             if(ispossible(mid,arr,k)){
                 ans = mid;
@@ -34,6 +32,7 @@ public:
                 st = mid+1;
             }
         }
-        return ans;
+        
+        return (int)ans;
     }
 };
