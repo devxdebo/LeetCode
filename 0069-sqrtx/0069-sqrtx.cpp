d9b711e7bac76061e6840b5f27964fcc4bd06e37
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-23 09:05:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.8%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,22 +4,22 @@ public:
         
         int st = 0;
         int end = x;
-        int ans;
+        long long ans;
 
         while(st<=end){
-            
+
             long long mid = st + (end-st)/2;
-            
-            if(mid*mid==x){
+
+            if(mid*mid == x){
                 return mid;
-            }else if(mid*mid<x){
-                ans = mid;
-                st = mid+1;
-            }else{
+            }else if(mid*mid > x){
                 end = mid-1;
+            }else{
+                st = mid+1;
+                ans = mid;
             }
         }
 
-        return ans;
+        return (int)ans;
     }
 };
