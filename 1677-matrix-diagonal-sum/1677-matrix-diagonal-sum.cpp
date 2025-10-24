24a37da08a15a0e5c38b36f430a5bb3d631c7a82
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-24 09:49:38 +0530

    Time: 0 ms (100%), Space: 14.9 MB (52.86%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    int diagonalSum(vector<vector<int>>& nums) {
-        
-        int N = nums[0].size();
-        long long sum = 0;
+    int diagonalSum(vector<vector<int>>& mat) {
+
+        int N = mat.size();
+        int endsize = N-1;
+        int sum = 0;
 
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
                 if(i==j){
-                    sum += nums[i][j];
+                    sum += mat[i][j];
                 }
             }
+            sum += mat[i][endsize];
+            endsize--;
         }
 
-        int x = 0;
-        int y = N-1;
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(i==x && j==y){
-                    sum += nums[i][j];
-                    x++;
-                    y--;
-                }
-            }
+        if(N%2!=0){
+            sum -= mat[N/2][N/2];
         }
 
-        if(N%2==0){
-            return sum;
-        }else{
-            return sum-nums[N/2][N/2];
-        }
+        return sum;
+        
     }
 };
