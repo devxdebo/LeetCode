19aef0bcf6ed9bfab602e717eb8c479db0267211
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-24 08:17:22 +0530

    Time: 0 ms (100%), Space: 25 MB (96.83%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -6,13 +6,17 @@ public:
         vector <int> ans;
 
         for(int i=0;i<N;i++){
+
             while(nums[i]!=i+1){
-                if(nums[i]==nums[nums[i]-1])
+
+                if(nums[i]==nums[nums[i]-1]){
                     break;
-                swap(nums[i],nums[nums[i]-1]);
+                }else{
+                    swap(nums[i],nums[nums[i]-1]);
+                }
             }
         }
-        
+
         for(int i=0;i<N;i++){
             if(nums[i]!=i+1){
                 ans.push_back(nums[i]);
