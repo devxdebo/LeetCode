7a0463fd1063486df1ca7007a034875cc3191d68
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-25 12:34:44 +0530

    Time: 0 ms (100%), Space: 26.1 MB (60.84%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
+
         int N = height.size();
-
         int maxheight = height[0];
-        int index = 0;
-
-        for(int i=0;i<N;i++){
-            if(height[i]>maxheight){
-                maxheight = height[i];
-                index = i;
-            }
-        }
-
-        int max1 = height[0];
+        int maxheightindex = 0;
+        int leftlargest = height[0];
+        int rightlargest = height[N-1];
         int water = 0;
 
-        for(int i=1;i<index;i++){
-            if(max1>height[i] && maxheight>height[i]){
-                water += min(max1,maxheight) - height[i];
-            }
-            if(height[i]>max1){
-                max1 = height[i];
+        for(int i=1;i<N;i++){
+            if(height[i]>maxheight){
+                maxheight = height[i];
+                maxheightindex = i;
             }
         }
 
-        max1 = height[N-1];
-
-        for(int i=N-2;i>index;i--){
-            if(max1>height[i] && maxheight>height[i]){
-                water += min(max1,maxheight) - height[i];
+        for(int i=1;i<maxheightindex;i++){
+            if(height[i]<=leftlargest){
+                water += leftlargest - height[i];
+            }else{
+                leftlargest = height[i];
             }
-            if(height[i]>max1){
-                max1 = height[i];
+        }
+
+        for(int i=N-2;i>maxheightindex;i--){
+            if(height[i]<=rightlargest){
+                water += rightlargest - height[i];
+            }else{
+                rightlargest = height[i];
             }
         }
 
         return water;
+
     }
 };
