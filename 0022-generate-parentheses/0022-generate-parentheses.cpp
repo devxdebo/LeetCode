7be295439eb58ca1482eaba71e2b5ed8120a5f67
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-29 14:04:09 +0530

    Time: 1 ms (86.92%), Space: 12.9 MB (88.09%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    void recursion(int n,int o,int c,string temp,vector <string> &ans){
-        
-        if(o==n && c==n){
-            ans.push_back(temp);
+    void helper(int o,int c,int n,string &temp,vector <string> &ans){
+        if(c==o && o==n){
+            ans.emplace_back(temp);
             return;
         }
-        if(o>=c && o!=n){
+        if(c>o){
+            return;
+        }
+        if(o<n){
             temp.push_back('(');
-            recursion(n,o+1,c,temp,ans);
+            helper(o+1,c,n,temp,ans);
             temp.pop_back();
         }
-        
-        if(o>c && c!=n){
+        if(c<n){
             temp.push_back(')');
-            recursion(n,o,c+1,temp,ans);
+            helper(o,c+1,n,temp,ans);
             temp.pop_back();
         }
     }
     vector<string> generateParenthesis(int n) {
+        
         string temp = "(";
-        int o = 1;
-        int c = 0;
         vector <string> ans;
-        recursion(n,o,c,temp,ans);
+        helper(1,0,n,temp,ans);
         return ans;
     }
 };
