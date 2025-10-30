93a78e4b0e5fff39ecd9f80013fdd067911e6e1f
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-30 09:02:57 +0530

    Time: 0 ms (100%), Space: 13.6 MB (42.08%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,17 +4,16 @@ public:
 
         int N = nums.size();
         int ans = N;
+
         int st = 0;
-        int end = N-1;;
-        
+        int end = N-1;
 
         while(st<=end){
 
             int mid = st + (end-st)/2;
 
             if(nums[mid]==target){
-                ans = mid;
-                break;
+                return mid;
             }else if(nums[mid]>target){
                 ans = mid;
                 end = mid-1;
@@ -22,7 +21,7 @@ public:
                 st = mid+1;
             }
         }
-
         return ans;
+        
     }
 };
